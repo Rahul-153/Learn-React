755647db1dd82da00bdf63f35647965e3c7d8d85
Rahul-153 <rathorerahul355@gmail.com>
2021-06-09 15:25:11 +0530

    deployed react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './Components/Card.js';
+
+function App() {
+    return ( 
+        <div className="main">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        </div>
+    );
+}
+
+export default App;
